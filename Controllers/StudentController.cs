1153b035a91cf1cd9dd8fb84140cf7688732cd4e
Boitshoko Mphahlele <boitshokontshoekgolo@gmail.com>
2025-02-28 10:46:21 +0200

    jeke

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -15,8 +15,8 @@ namespace BoitshokoStudyGroup.Controllers
             students.Add(new Models.Student { Student_Number = "u24634400", StudentName = "Boitshoko", Surname = "Mphahlele", Email = "u24634400@tuks.co.za" });
             students.Add(new Models.Student { Student_Number = "u24784606", StudentName = "Msizi", Surname = "Gumede", Email = "u24784606@tuks.co.za" });
             students.Add(new Models.Student { Student_Number = "u24924718", StudentName = "Gundo", Surname = "Ndou", Email = "u24924718@tuks.co.za" });
-            students.Add(new Models.Student { Student_Number = "u24717140", StudentName = "Ritshidze", Surname = "Maphanga", Email = "u24717140@tuks.co.za" });
-            students.Add(new Models.Student { Student_Number = "u24657123", StudentName = "Jack", Surname = "Smith", Email = "u24657123@tuks.co.za" });
+            students.Add(new Models.Student { Student_Number = "u24717739", StudentName = "Ritshidze", Surname = "Nedzimauli", Email = "u24717739@tuks.co.za" });
+            students.Add(new Models.Student { Student_Number = "u24758354", StudentName = "Nhlanhla", Surname = "Mnisi", Email = "u24758354@tuks.co.za" });
 
 
             return View(students);
